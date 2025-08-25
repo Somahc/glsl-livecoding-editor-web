3ac793bce0b876bfe64773370701bf6683dea77f
Somahc <sekine.soma.ss@gmail.com>
2025-08-25 12:56:13 +0900

    fsだけホットスワップするよう置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import Editor from "./components/Editor";
 import ShaderCanvas from "./components/ShaderCanvas";
 import { useState } from "react";
 
-const DEFAULT_FS_CODE = `#version 300 es 
+export const DEFAULT_FS_CODE = `#version 300 es 
 precision highp float; out vec4 o;
 uniform vec2 r; uniform float t;
 
-void main(){ vec2 uv=(gl_FragCoord.xy-0.5*r)/r.y; float v=.02/abs(length(uv)-.5+.1*sin(t)); o=vec4(vec3(v),1.); }`;
+void main()
+{
+  vec2 uv=(gl_FragCoord.xy-0.5*r)/r.y;
+  float v=.02/abs(length(uv)-.5+.1*sin(t));
+  o=vec4(vec3(v),1.);
+}`;
 
 function App() {
   const [code, setCode] = useState<string>(DEFAULT_FS_CODE);
-  const [fsSource, setFsSource] = useState<string | null>(null);
+  const [fsSource, setFsSource] = useState<string | null>(DEFAULT_FS_CODE);
 
   const handleSave = () => {
     setFsSource(code);
