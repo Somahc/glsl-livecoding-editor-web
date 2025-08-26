639473d55e207ae0e2a5d04ecee712899b48c447
Soma Sekine <a8121151@aoyama.jp>
2025-08-26 18:37:02 +0900

    monthが0-basedだったのを修正

--- src/components/UpperTimeStatsPanel/UpperTimeStatsPanel.tsx
+++ src/components/UpperTimeStatsPanel/UpperTimeStatsPanel.tsx
@@ -27,7 +27,7 @@ const UpperTimeStatsPanel = () => {
   function getCurrentTimeFormatted() {
     const now = new Date();
     const year = now.getFullYear().toString();
-    const month = now.getMonth().toString().padStart(2, "0");
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
     const day = now.getDate().toString().padStart(2, "0");
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
