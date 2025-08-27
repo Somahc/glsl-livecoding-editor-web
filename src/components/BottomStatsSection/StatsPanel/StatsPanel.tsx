ed1950e363d252d6363039644665eee2fc3bb6ad
Somahc <sekine.soma.ss@gmail.com>
2025-08-27 19:12:24 +0900

    beat可視メーター仮実装

--- src/components/BottomStatsSection/StatsPanel/StatsPanel.tsx
+++ src/components/BottomStatsSection/StatsPanel/StatsPanel.tsx
@@ -1,20 +1,24 @@
 import bottomCommonStyle from "../index.module.css";
 import { useCurrentStatsInfo } from "../../../stores/useCurrentStatsInfo";
+import style from "./index.module.css";
 
 export default function StatsPanel() {
-  const { shaderBPM, setShaderBPM, setIsResetShaderTime } = useCurrentStatsInfo();
+  const { shaderBPM, setShaderBPM, setIsResetShaderTime, currentElapsedTime } = useCurrentStatsInfo();
 
   const handleBPMChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setShaderBPM(Number(e.target.value));
   };
 
-  const handleChangeBPMTo240 = () => {
-    setShaderBPM(240);
-  };
-
   const handleResetShaderTime = () => {
     setIsResetShaderTime(true);
   };
+
+  const beatMeter = () => {
+    const elapsedTime = currentElapsedTime * (shaderBPM / 60);
+    const bar = Math.floor(elapsedTime);
+    const beat = elapsedTime - bar;
+    return 100 - beat * 100;
+  }
 
   return (
     <div className={bottomCommonStyle.BottomPanelcontainer}>
@@ -22,10 +26,13 @@ export default function StatsPanel() {
         <input type="number" min={0} max={240} value={shaderBPM} onChange={handleBPMChange} />
       </div>
       <div>
-        <button onClick={handleChangeBPMTo240}>change BPM to 240</button>
+        <button onClick={handleResetShaderTime}>Reset shader time</button>
       </div>
       <div>
-        <button onClick={handleResetShaderTime}>Reset shader time</button>
+        <div>BEAT</div>
+        <div className={style.beatMeter}>
+          <div className={style.beatMeterBar} style={{ transform: `translateX(-${beatMeter()}%)` }}></div>
+        </div>
       </div>
     </div>
   );
